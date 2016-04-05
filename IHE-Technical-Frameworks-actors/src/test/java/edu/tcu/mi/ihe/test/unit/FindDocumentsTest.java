f3eac03914ea0cd806858fa8affeae18da198cb4
Gaduo <foxb1249@gmail.com>
2016-04-05 20:56:13 +0800

    fix bug

--- IHE-Technical-Frameworks-actors/src/test/java/edu/tcu/mi/ihe/test/unit/FindDocumentsTest.java
+++ IHE-Technical-Frameworks-actors/src/test/java/edu/tcu/mi/ihe/test/unit/FindDocumentsTest.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.junit.Test;
 
 import edu.tcu.mi.ihe.iti.ebxml.query.AdhocQueryResponseType;
-import edu.tcu.mi.ihe.iti.ebxml.rim.ExtrinsicObjectType;
+import edu.tcu.mi.ihe.iti.ebxml.rim.ObjectRefType;
 import edu.tcu.mi.ihe.iti.ebxml.rim.RegistryObjectListType;
 import edu.tcu.mi.ihe.iti.model.FindDocuments;
 import edu.tcu.mi.ihe.test.RegsitryStoredQueryTest;
@@ -29,15 +29,13 @@ public class FindDocumentsTest extends RegsitryStoredQueryTest {
 	@Override
 	protected boolean validation(AdhocQueryResponseType adhocQueryResponse) {
 		RegistryObjectListType objectList = adhocQueryResponse.getRegistryObjectList();
-		List<ExtrinsicObjectType> extrinsicObjects = objectList.getExtrinsicObjects();
-		boolean found = false;
-		for(ExtrinsicObjectType extrinsicObject : extrinsicObjects){
-			if(extrinsicObject.getId().equals(documentId)){
-				found = true;
-				break;
+		List<ObjectRefType> ids = objectList.getObjectRefs();
+		for(ObjectRefType id : ids){
+			if(id.getId().equalsIgnoreCase(documentId)){
+				return true;
 			}
 		}
-		return found;
+		return false;
 	}
 
 }
